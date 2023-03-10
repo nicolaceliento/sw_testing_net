fe9f95865f39a9d8c7bbce99452c5f7e4b8c43cf
Nicola Celiento <nicola.celiento@inps.it>
2023-03-10 09:26:36 +0100

    rimozione errori UT

--- ShoppingCart/Services/ShoppingCartServiceFake.cs
+++ ShoppingCart/Services/ShoppingCartServiceFake.cs
@@ -16,10 +16,10 @@ namespace ShoppingCart
         {
             _shoppingCart = new List<ShoppingItem>()
             {
-                //new ShoppingItem() { Id = new Guid("ab2bd817-98cd-4cf3-a80a-53ea0cd9c200"),
-                //    Name = "Orange Juice", Manufacturer="Orange Tree", Price = 5.00M },
-                //new ShoppingItem() { Id = new Guid("815accac-fd5b-478a-a9d6-f171a2f6ae7f"),
-                //    Name = "Diary Milk", Manufacturer="Mad Cow", Price = 4.00M },
+                new ShoppingItem() { Id = new Guid("ab2bd817-98cd-4cf3-a80a-53ea0cd9c200"),
+                    Name = "Orange Juice", Manufacturer="Orange Tree", Price = 5.00M },
+                new ShoppingItem() { Id = new Guid("815accac-fd5b-478a-a9d6-f171a2f6ae7f"),
+                    Name = "Diary Milk", Manufacturer="Mad Cow", Price = 4.00M },
                 new ShoppingItem() { Id = new Guid("33704c4a-5b87-464c-bfb6-51971b4d18ad"),
                     Name = "Frozen Pizza", Manufacturer="Uncle Mickey's", Price = 12.00M }
             };
@@ -27,17 +27,17 @@ namespace ShoppingCart
 
         public IEnumerable<ShoppingItem> GetAllItems()
         {
-            //check code smell
-            string test;
-            if (true)
-            {
-                myIP = "192.168.1.1";
-            }
-            else
-            {
-                //Test coverage
-                myIP = "192.168.1.1";
-            }
+            ////check code smell
+            //string test;
+            //if (true)
+            //{
+            //    myIP = "192.168.1.1";
+            //}
+            //else
+            //{
+            //    //Test coverage
+            //    myIP = "192.168.1.1";
+            //}
 
             return _shoppingCart;
         }
